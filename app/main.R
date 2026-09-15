11cc8b676192a1fb6e0b9887800fa343182bf64c
clbenoit <benoitclement.sand@gmail.com>
2026-09-15 09:50:23 +0200

    remove Myogre from analysis choices

--- app/main.R
+++ app/main.R
@@ -64,7 +64,8 @@ ui <- function(id) {
                     selectizeInput(
                       ns("analysis_name"),
                       label = "Analysis selector", # Traduit: "Sélecteur d'analyse"
-                      choices = c("Myogre", "Exomes", "TS65/GHEM-FFPE", "Hema_M_L_CHUGA", "Hedera"),
+                      #choices = c("Myogre", "Exomes", "TS65/GHEM-FFPE", "Hema_M_L_CHUGA", "Hedera"),
+                      choices = c("Exomes", "TS65/GHEM-FFPE", "Hema_M_L_CHUGA", "Hedera"),
                       selected = "TS65/GHEM-FFPE",
                       width = "100%"
                     )
